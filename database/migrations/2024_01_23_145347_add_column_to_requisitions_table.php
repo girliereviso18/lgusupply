d338d9432aaaceebc30f36117de19c4d00ad4e8d
Girlie <revisolala@gmail.com>
2024-01-23 22:58:28 +0800

    1/23/24

---
+++ database/migrations/2024_01_23_145347_add_column_to_requisitions_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('requisitions', function (Blueprint $table) {
+            $table->integer('admin_new_approved')->default(1);
+            $table->integer('admin_new_disapproved')->default(1);
+            $table->integer('employee_new_approved')->default(1);
+            $table->integer('employee_new_disapproved')->default(1);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('requisitions', function (Blueprint $table) {
+            //
+        });
+    }
+};
