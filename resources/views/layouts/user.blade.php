7c5f5b40649263801f1b334752a830c5df3b00ec
Girlie <revisolala@gmail.com>
2024-01-24 13:19:14 +0800

    1/24/24

--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -101,7 +101,7 @@
           <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
           </li>
           <li class="nav-item d-none d-sm-inline-block">
-            <a href="" class="nav-link" style="font-weight: bold;">Municipality of Bontoc Supply Management System-USER</a>
+            <a href="" class="nav-link" style="font-weight: bold;">Municipality of Bontoc Supply Management System-User</a>
 </li>
 
         </ul>
